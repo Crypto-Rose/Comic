2c76f7619bac8eaa7f9e7c3131a01338c9a64b80
Rosa Morales <05rosa04@gmail.com>
2021-02-20 09:29:48 -0500

    Termined x2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Container,Row,Col, } from 'react-bootstrap';
 import Axios from 'axios';
-import cors from 'cors';
 import Comic from './components/Comic'
 import Vote from './components/Vote'
 
@@ -11,7 +10,7 @@ function App() {
     const [imgData,setImgData] = useState([]);
 
     useEffect(() => {
-        Axios.get('https://xkcd.com/566/info.0.json',cors())
+        Axios.get('https://xkcd.com/566/info.0.json')
         .then(response => response)                   
         .then(data => setImgData(data.data))
                       
